579686d951722994b7a49ddb1ad2dfb0aeb5cdee
Geoff S <122327227+Deryaus@users.noreply.github.com>
2023-03-17 11:49:18 -0400

    Update apod_viewer.py
    Messing around with tkinter

--- apod_viewer.py
+++ apod_viewer.py
@@ -6,9 +6,11 @@
 --------------------ι𝐍Ⓙย𝐬𝓣ᶤςⒺ Ⓐ𝐍ＹωᕼⒺг𝐄 ᶤ𝐬 ᵃ tｈяᗴＡт ⓉＯ 𝐣υ𝔰ｔ𝐢ᶜⓔ 𝐄V乇яｙ山卄εŘ乇 --------------------"""
 
 from tkinter import *
+from tkinter import ttk
 import inspect
 import os
 import apod_desktop
+from PIL import Image, ImageTk
 
 
 # Determine the path and parent directory of this script
@@ -22,4 +24,14 @@ apod_desktop.init_apod_cache(script_dir)
 root = Tk()
 root.geometry('600x400')
 
+root.title('Astronomy Picture Of The Day')
+root.iconbitmap(r'C:\temp\favicon.ico')
+bckgrd_image = Image.open(r'C:\temp\images\19.jpg')
+bckgrd_image = bckgrd_image.resize((root.winfo_screenwidth(), root.winfo_screenheight()))
+bphoto = ImageTk.PhotoImage(bckgrd_image)
+label = Label(root, image=bphoto, width=600, height=400)
+button = Button(root, text='something somethin')
+button.place(x=350,y=350,)
+
+label.pack()
 root.mainloop()
